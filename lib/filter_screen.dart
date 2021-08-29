21ddf825cd42f37d3475199c2123fcbd0e6ed214
AnasKhaled1876 <Anas.khaled1892@gmail.com>
2021-08-29 23:56:21 +0200

    Added RangeSlider

--- lib/filter_screen.dart
+++ lib/filter_screen.dart
@@ -8,6 +8,10 @@ class FilterScreen extends StatefulWidget {
 }
 
 class _FilterScreenState extends State<FilterScreen> {
+  int min=800000;
+  int max = 2000000;
+  var selectedRange = RangeValues(800000, 2000000);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,20 +21,44 @@ class _FilterScreenState extends State<FilterScreen> {
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
           Container(
-            margin: EdgeInsets.only(top: 30.0),
+            margin: EdgeInsets.only(top: 30.0, left: 20.0),
             child: Text("Design",
                 style: TextStyle(
                     fontWeight: FontWeight.bold,
-                    fontSize: 40.0,
+                    fontSize: 35.0,
                     color: Colors.black)),
           ),
           Container(
-          margin: EdgeInsets.only(left: 25.0),
+            margin: EdgeInsets.only(left: 20.0, top: 5.0),
             child: Text(
-              "your Home freely",
-              style: TextStyle(fontSize: 35.0),
+              "your Home",
+              style: TextStyle(fontSize: 30.0),
             ),
           ),
+          Container(margin: EdgeInsets.only(top: 30.0),
+            alignment: Alignment.topRight,
+            child: Text(
+              "$min - $max",
+              style: TextStyle(fontSize: 25.0),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.all(20.0),
+            child: RangeSlider(
+              values: selectedRange,
+              onChanged: (RangeValues newValues) {
+                setState(() {
+                  selectedRange = newValues;
+                  min=newValues.start.toInt();
+                  max=newValues.end.toInt();
+                });
+              },
+              max: 2000000,
+              min: 800000,
+              labels:
+                  RangeLabels('${selectedRange.start}', '${selectedRange.end}'),
+            ),
+          )
         ],
       ),
     );
