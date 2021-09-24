e5c0c04d9ca98b64954c5b311c0c6b3a1d5a3791
AnasKhaled1876 <Anas.khaled1892@gmail.com>
2021-09-24 14:07:53 +0200

    Added Item animation

--- lib/screens/designs_screen.dart
+++ lib/screens/designs_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animations/animations.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -40,47 +41,50 @@ class _DesignsScreenState extends State<DesignsScreen> {
         backgroundColor: Colors.black,
       ),
       backgroundColor: Constants.backgroundColor,
-      body: Column(mainAxisAlignment: MainAxisAlignment.center,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-        Expanded(
-          child: ListView(
-            itemExtent: 180,
-            children: designCards,
-            scrollDirection: Axis.vertical,
+          Expanded(
+            child: ListView(
+              itemExtent: 180,
+              children: designCards,
+              scrollDirection: Axis.vertical,
+            ),
           ),
-        ),
-        if(widget.guest)
-          Container(margin: EdgeInsets.symmetric(vertical: 15.0),
-            child: GestureDetector(
-              child: Center(
-                child: const Text(
-                  "For more options and Designs\n Sign Up",
-                  style: TextStyle(fontSize: 23.0, color: Colors.white),
-                textAlign: TextAlign.center,
+          if (widget.guest)
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15.0),
+              child: GestureDetector(
+                child: Center(
+                  child: const Text(
+                    "For more options and Designs\n Sign Up",
+                    style: TextStyle(fontSize: 23.0, color: Colors.white),
+                    textAlign: TextAlign.center,
+                  ),
                 ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return const StartScreen();
+                  }));
+                },
               ),
-              onTap: () {
+            )
+        ],
+      ),
+      floatingActionButton: !widget.guest
+          ? FloatingActionButton(
+              backgroundColor: Colors.black,
+              onPressed: () {
                 Navigator.push(context, MaterialPageRoute(builder: (context) {
-                  return const StartScreen();
+                  return FilterScreen();
                 }));
               },
-            ),
-          )
-      ],
-      ),
-      floatingActionButton: !widget.guest ? FloatingActionButton(
-        backgroundColor: Colors.black,
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return FilterScreen();
-          }));
-        },
-        child:
-          Icon(
-          FontAwesomeIcons.filter,
-          color: Constants.roqi,
-        ),
-      ) : null,
+              child: Icon(
+                FontAwesomeIcons.filter,
+                color: Constants.roqi,
+              ),
+            )
+          : null,
     );
   }
 }
@@ -93,113 +97,123 @@ class HouseItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.all(9.0),
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) {
-            return HouseScreen(house: design);
-          }));
-        },
-        child: Row(children: <Widget>[
-          Expanded(
-            flex: 1,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: CachedNetworkImage(
-                imageUrl: design.url,
-                placeholder: (context, url) => LoadingIndicator(
-                    indicatorType: Indicator.ballClipRotateMultiple,
-                    colors: const [Constants.roqi],
-                    strokeWidth: 2,
-                    backgroundColor: Colors.black,
-                    pathBackgroundColor: Colors.black),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-              ),
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Center(
-                child: Text(
-                  "  AED  ${f.format(design.price)}",
-                  style: TextStyle(
-                      fontSize: 28.0,
-                      fontWeight: FontWeight.w400,
-                      color: Colors.white),
+    return OpenContainer(
+      transitionDuration: Duration(milliseconds: 550),
+      closedColor: Constants.backgroundColor,
+      openColor: Constants.backgroundColor,
+      middleColor: Constants.backgroundColor,
+      transitionType: ContainerTransitionType.fadeThrough,
+      openBuilder:
+          (BuildContext context, void Function({Object? returnValue}) action) {
+        return HouseScreen(house: design);
+      },
+      closedBuilder: (BuildContext context, void Function() action) {
+        return Padding(
+          padding: EdgeInsets.all(9.0),
+          child: Row(children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(8.0),
+                child: CachedNetworkImage(
+                  imageUrl: design.url,
+                  placeholder: (context, url) => LoadingIndicator(
+                      indicatorType: Indicator.ballClipRotateMultiple,
+                      colors: const [Constants.roqi],
+                      strokeWidth: 2,
+                      backgroundColor: Colors.black,
+                      pathBackgroundColor: Colors.black),
+                  errorWidget: (context, url, error) => Icon(Icons.error),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Container(
-                    height: 60.0,
-                    width: 90.0,
-                    child: ListTile(
-                      leading: Icon(
-                        FontAwesomeIcons.bed,
-                        color: Colors.white,
-                      ),
-                      title: Text("${design.bedroom}",
-                          style:
-                              TextStyle(fontSize: 20.0, color: Colors.white)),
-                    ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Center(
+                  child: Text(
+                    "  AED  ${f.format(design.price)}",
+                    style: TextStyle(
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.w400,
+                        color: Colors.white),
                   ),
-                  Container(
-                    width: 120.0,
-                    height: 60.0,
-                    child: ListTile(
-                      leading: Icon(
-                        FontAwesomeIcons.bath,
-                        color: Colors.white,
-                      ),
-                      title: Center(
-                        child: Text("${design.bathroom}",
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(
+                      height: 60.0,
+                      width: 90.0,
+                      child: ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.bed,
+                          color: Colors.white,
+                        ),
+                        title: Text("${design.bedroom}",
                             style:
                                 TextStyle(fontSize: 20.0, color: Colors.white)),
                       ),
                     ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Container(
-                    width: 90.0,
-                    height: 60.0,
-                    child: ListTile(
-                      leading: Icon(
-                        FontAwesomeIcons.couch,
-                        color: Colors.white,
-                      ),
-                      title: Text("${design.majls}",
-                          style:
-                              TextStyle(fontSize: 20.0, color: Colors.white)),
-                    ),
-                  ),
-                  Container(
-                    height: 60.0,
-                    width: 120.0,
-                    child: ListTile(
-                      leading: Icon(
-                        FontAwesomeIcons.warehouse,
-                        color: Colors.white,
-                      ),
-                      title: Text(
-                        "  ${design.store}",
-                        style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    Container(
+                      width: 120.0,
+                      height: 60.0,
+                      child: ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.bath,
+                          color: Colors.white,
+                        ),
+                        title: Center(
+                          child: Text("${design.bathroom}",
+                              style: TextStyle(
+                                  fontSize: 20.0, color: Colors.white)),
+                        ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ]),
-      ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      width: 90.0,
+                      height: 60.0,
+                      child: ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.couch,
+                          color: Colors.white,
+                        ),
+                        title: Text("${design.majls}",
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.white)),
+                      ),
+                    ),
+                    Container(
+                      height: 60.0,
+                      width: 120.0,
+                      child: ListTile(
+                        leading: Icon(
+                          FontAwesomeIcons.warehouse,
+                          color: Colors.white,
+                        ),
+                        title: Text(
+                          "  ${design.store}",
+                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ]),
+        );
+      },
     );
   }
 }
+
+// Navigator.push(context, MaterialPageRoute(builder: (context) {
+// return HouseScreen(house: design);
+// }));
